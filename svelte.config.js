33590600b134e27220c5de705a2ada6477cf4bec
Saterz_ <tilouc93@gmail.com>
2025-06-20 09:30:31 +0100

    fix: update SvelteKit adapter to use Vercel instead of Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import netlify from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: netlify() }
+  preprocess: vitePreprocess(),
+  kit: { adapter: adapter() },
 };
 
 export default config;
